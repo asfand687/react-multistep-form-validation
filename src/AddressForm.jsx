2a197b3e6b47fb643570973e95296ad04407439e
asfand687 <asfandyar687@gmail.com>
2023-02-15 00:03:10 +0500

    completed form validation

--- src/AddressForm.jsx
+++ src/AddressForm.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import FormWrapper from './FormWrapper'
 
-const AddressForm = () => {
+const AddressForm = ({ street, city, state, zip, updateFields }) => {
   return (
     <FormWrapper title="Address">
       <label>Street</label>
-      <input autoFocus required type="text" />
+      <input value={street} onChange={e => updateFields({ street: e.target.value })} autoFocus required type="text" />
       <label>City</label>
-      <input required type="text" />
+      <input value={city} onChange={e => updateFields({ city: e.target.value })} required type="text" />
       <label>State</label>
-      <input required type="text" />
+      <input value={state} onChange={e => updateFields({ state: e.target.value })} required type="text" />
       <label>Zip</label>
-      <input required type="text" />
+      <input value={zip} onChange={e => updateFields({ zip: e.target.value })} required type="text" />
     </FormWrapper>
   )
 }
