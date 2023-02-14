4f4c7dc5762b4ec8982033d30c8db9376b19299e
asfand687 <asfandyar687@gmail.com>
2023-02-15 00:03:15 +0500

    completed form validation

--- src/UserForm.jsx
+++ src/UserForm.jsx
@@ -1,15 +1,32 @@
 import React from 'react'
 import FormWrapper from './FormWrapper'
 
-const UserForm = () => {
+const UserForm = ({ firstName, lastName, age, updateFields }) => {
   return (
     <FormWrapper title="User Details">
       <label>First Name</label>
-      <input autoFocus required type="text"/>
+      <input
+        autoFocus
+        required
+        type="text"
+        value={firstName}
+        onChange={e => updateFields({ firstName: e.target.value })}
+      />
       <label>Last Name</label>
-      <input required type="text" />
+      <input
+        value={lastName}
+        required
+        type="text"
+        onChange={e => updateFields({ lastName: e.target.value })}
+      />
       <label>Age</label>
-      <input min={1} required type="number" />
+      <input
+        min={1}
+        required
+        type="number"
+        value={age}
+        onChange={e => updateFields({ age: e.target.value })}
+      />
     </FormWrapper>
   )
 }
